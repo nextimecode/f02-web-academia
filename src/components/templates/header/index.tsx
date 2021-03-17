0dd3b38e5fb534fc7bdca5704181e2497597cff2
Marcus Lamounier <marcus_quadros@hotmail.com>
2021-03-16 22:28:29 -0300

    header

--- src/components/templates/header/index.tsx
+++ src/components/templates/header/index.tsx
@@ -31,7 +31,7 @@ const Header = () => {
         }
     ]
 
-    return <Menu urlLogo={urlLogo} listNavLinks={listNavLinks} />
+    return <header><Menu urlLogo={urlLogo} listNavLinks={listNavLinks} /></header>
 }
 
 export default Header
